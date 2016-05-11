14f5c0a6d53e5957ee8e7ffb9bd957e459ab65bf
Armin <armin.bagheri.1380@gmail.com>
2016-05-11 22:46:27 +0430

    Update ID.lua

--- plugins/ID.lua
+++ plugins/ID.lua
@@ -1,19 +1,13 @@
 do
-
 function run(msg, matches)
-local reply_id = msg['id']
-
-local info = '#Group ID: '..msg.to.id..' \n #User ID: '..msg.from.id
-
-reply_msg(reply_id, info, ok_cb, false)
-end
-
+  return " Group ID: '..msg.to.id..'\nUser ID: '..msg.from.id
+  end
 return {
-patterns = {
-"^[!/#][Ii][Dd]"
-
-},
-run = run
+  description = " ID ", 
+  usage = " ID ",
+  patterns = {
+    "^[#/!][Ii][Dd]$",
+  },
+  run = run
 }
-
 end
