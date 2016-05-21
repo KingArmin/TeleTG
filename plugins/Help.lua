051e8d8b5ae1f64b0bcdd9d3db92a4c3417ab917
Armin <armin.bagheri.1380@gmail.com>
2016-05-21 12:54:05 +0430

    Update Help.lua

--- plugins/Help.lua
+++ plugins/Help.lua
@@ -1,36 +1,81 @@
 local function run(msg, matches)
   if is_chat_msg(msg) then
-    local text = [[
-    
-  FirsName: "..msg.from.first_name.."
-  LastName: "..msg.from.last_name.."
-  
-  Username: @"..(msg.from.username or '').."
-  UserID: "..msg.from.id.."
-  Phone: "..msg.from.phone.."
-  
-  Group Name: "..msg.to.title.."
-  Group ID: "..msg.to.id.."
-  Total Messages: "..user_info_msgs.."
+    local text = [[‌✅Commands to lock|unlock
+💭/close|open link
+💭/close|open member
+💭/close|open name
+💭/close|open bot
+💭/close|open photo
+💭/close|open sticker
 
-]]
+➕
+✅Commands for control member
+💭/kick
+💭/ban
+💭/unban
+💭/kickme
+
+➕
+✅Group control 
+💭/rules
+💭/setrules 
+💭/about
+💭/setabout 
+💭/setphoto 
+💭/setname 
+💭/id
+💭/group settings 
+💭/modlist
+💭/help
+
+➕
+✅ Group Promote commands
+💭/spromote
+💭/sdemote 
+💭/promote 
+💭/demote
+
+➖🔸➖🔹➖🔸➖🔹➖]]
     return text
   end
   if is_channel_msg(msg) then
-    local text = [[
-    
-  FirsName: "..msg.from.first_name.."
-  LastName: "..msg.from.last_name.."
-  
-  Username: @"..(msg.from.username or '').."
-  UserID: "..msg.from.id.."
-  Phone: "..msg.from.phone.."
-  
-  SuperGroup Name: "..msg.to.title.."
-  SuperGroup ID: "..msg.to.id.."
-  Total Messages: "..user_info_msgs.."
-  
-    ]]
+    local text = [[‌‌✅Commands For lock|unlock
+💭 #lock|unlock all
+💭 #lock|unlock link
+💭 #lock|unlock member
+💭 #lock|unlock image
+💭 #lock|unlock sticker
+💭 #lock|unlock forward
+ 
+➕
+✅Commands For Control Member
+💭 #kick @username
+💭 #ban @username
+💭 #unban @username
+💭 #kickme @username
+💭 #mute @username
+
+➕
+✅Group Control 
+💭 #rules
+💭 #setrules [Text]
+💭 #about
+💭 #setabout [Text]
+💭 #setphoto 
+💭 #setname [Name]
+💭 #id
+💭 #group settings 
+💭 #modlist
+💭 #help
+
+➕
+✅ Group Promote Commands
+💭 #spromote
+💭 #sdemote
+💭 #promote
+💭 #demote
+
+➖🔸➖🔹➖🔸➖🔹➖]]
     return text
   else
     local text = [[aaa]]
@@ -40,7 +85,7 @@ end
 
 return {
   patterns = {
-    "^[#!/][Mm][Ee]$",
+    "^[#!/](help)$",
   }, 
   run = run,
 }
